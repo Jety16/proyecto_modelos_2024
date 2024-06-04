3d958ab3c5ee20961601bc9ff7ced9b2d90e2165
Tomas Hubmann <thubmann@seenka.com>
2024-06-04 10:27:51 -0300

    update add statics and results.txt

--- Hubmann-PereyraCarrillo.py
+++ Hubmann-PereyraCarrillo.py
@@ -2,8 +2,6 @@ from random import random
 import numpy as np
 import math
 
-INF = 2**20
-
 def sistema_rep_gen(N, S, OP):
     avaiable = N + S  # Cajas disponibles
     avaiable_op = OP  # Operarios disponibles
@@ -14,30 +12,29 @@ def sistema_rep_gen(N, S, OP):
 
     for _ in range(OP):
         # inicializacion de los tiempos de reparacion de las cajas
-        repaird_moment.append(INF)
+        repaird_moment.append(np.inf)
 
     for _ in range(N):
         # Generamos las N exponenciales y las colocamos en la lista break_moment
         # Estas Exponenciales denotan los tiempos donde las cajas tuvieron defectos
         break_moment.append((-math.log(random())))
-    break_moment.sort()
 
     while avaiable >= N:  # Verificamos guarda de fallo del sistema, si tenemos menos de N maquinas funcionales, paramos
         min_repair_position = repaird_moment.index(min(repaird_moment))
-        if break_moment[0] <= repaird_moment[min_repair_position]:
+        min_break_position = break_moment.index(min(break_moment))
+        if break_moment[min_break_position] <= repaird_moment[min_repair_position]:
             # Se Rompio una maquina antes de que se termine de reparar una
             to_repair += 1
             avaiable -= 1
-            sim_time = break_moment[0]
-            break_moment[0] = sim_time - math.log(random())
-            break_moment.sort()
+            sim_time = break_moment[min_break_position]
+            break_moment[min_break_position] = sim_time - math.log(random())
         else:
             # Se termino de reparar una maquina de que se rompa otra
             to_repair -= 1
             avaiable += 1
             avaiable_op += 1
             sim_time = repaird_moment[min_repair_position]
-            repaird_moment[min_repair_position] = INF
+            repaird_moment[min_repair_position] = np.inf
 
         if avaiable_op > 0 and to_repair > 0:
             # Hay maquinas para reparar y el operario esta libre
@@ -52,50 +49,23 @@ def sistema_rep_ej1(N, S):
 
 # Calculo esperanza, variacion y desviacion estandar de los distintos casos de estudio
 
-e = 0
-sum_cuadrados = 0
-n_sim = 10_000
-for _ in range(n_sim):
-    res_sim = sistema_rep_ej1(7, 3)
-    e += res_sim
-    sum_cuadrados += res_sim**2
-esperanza = e/ n_sim
-varianza = (sum_cuadrados/n_sim - esperanza**2)
-print("Caso de estudio con 1 operarios, 7 maquinas en uso y 3 repuestos")
-print(f"Esperanza: {esperanza}")
-print(f"Varianza: {varianza}")
-print(f"Desviacion estandar: {np.sqrt(varianza)}")
-print('------------------------')
+def statics(N,S,OP):
+    sum = 0
+    sum_cuadrados = 0
+    n_sim = 10_000
+    for _ in range(n_sim):
+        res_sim = sistema_rep_gen(N, S, OP)
+        sum += res_sim
+        sum_cuadrados += res_sim**2
+    esperanza = sum/ n_sim
+    varianza = (sum_cuadrados/n_sim - esperanza**2)
+    print(F"\nCaso de estudio con {OP} operarios, {N} maquinas en uso y {S} repuestos")
+    print(f"Esperanza: {esperanza}")
+    print(f"Varianza: {varianza}")
+    print(f"Desviacion estandar: {np.sqrt(varianza)}")
+    print('------------------------')
 
+casos_de_estudio = [[7,3,1], [7,3,2], [7,4,1]]
 
-
-e = 0
-sum_cuadrados = 0
-for _ in range(n_sim):
-    res_sim = sistema_rep_gen(7, 3, 2)
-    e += res_sim
-    sum_cuadrados += res_sim**2
-esperanza = e/ n_sim
-varianza = (sum_cuadrados/n_sim - esperanza**2)
-print("Caso de estudio con 2 operarios, 7 maquinas en uso y 3 repuestos")
-print(f"Esperanza: {esperanza}")
-print(f"Varianza: {varianza}")
-print(f"Desviacion estandar: {np.sqrt(varianza)}")
-print('------------------------')
-
-e = 0
-sum_cuadrados = 0
-for _ in range(n_sim):
-    res_sim = sistema_rep_gen(7, 4, 1)
-    e += res_sim
-    sum_cuadrados += res_sim**2
-esperanza = e/ n_sim
-varianza = (sum_cuadrados/n_sim - esperanza**2)
-print("Caso de estudio con 1 operarios, 7 maquinas en uso y 4 repuestos")
-print(f"Esperanza: {esperanza}")
-print(f"Varianza: {varianza}")
-print(f"Desviacion estandar: {np.sqrt(varianza)}")
-print('------------------------')
-
-
-
+for value in casos_de_estudio:
+    statics(*value)
