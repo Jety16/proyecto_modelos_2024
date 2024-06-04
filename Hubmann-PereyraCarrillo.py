5d2e7de6b74eaa9aab0d1ef9dbb736acb6b95e0b
Tomas Hubmann <thubmann@seenka.com>
2024-06-04 09:39:46 -0300

    clean code

--- Hubmann-PereyraCarrillo.py
+++ Hubmann-PereyraCarrillo.py
@@ -4,17 +4,16 @@ import math
 
 INF = 2**20
 
-
-
-def sistema_rep_gen(N, S, O):
+def sistema_rep_gen(N, S, OP):
     avaiable = N + S  # Cajas disponibles
-    avaiable_op = O  # Operarios disponibles
+    avaiable_op = OP  # Operarios disponibles
     to_repair = 0  # Cajas a reparar
     break_moment = []  # Lista de los tiempos en los que las cajas tuvieron defectos
-    repaird_moment = [] # Momento en el q
+    repaird_moment = [] # Momento en el que los operarios terminan de reparar las cajas
     sim_time = 0 # Denota el paso del tiempo dentro de la simulacion
 
-    for _ in range(O):
+    for _ in range(OP):
+        # inicializacion de los tiempos de reparacion de las cajas
         repaird_moment.append(INF)
 
     for _ in range(N):
@@ -24,11 +23,10 @@ def sistema_rep_gen(N, S, O):
     break_moment.sort()
 
     while avaiable >= N:  # Verificamos guarda de fallo del sistema, si tenemos menos de N maquinas funcionales, paramos
-        ## print("\n", avaiable, break_moment[0], repaird_moment, to_repair, sim_time)
         min_repair_position = repaird_moment.index(min(repaird_moment))
         if break_moment[0] <= repaird_moment[min_repair_position]:
-            to_repair += 1
             # Se Rompio una maquina antes de que se termine de reparar una
+            to_repair += 1
             avaiable -= 1
             sim_time = break_moment[0]
             break_moment[0] = sim_time - math.log(random())
@@ -46,12 +44,13 @@ def sistema_rep_gen(N, S, O):
             max_position = repaird_moment.index(max(repaird_moment))
             repaird_moment[max_position] = sim_time - (1/8 * math.log(random()))
             avaiable_op -= 1
-        ## print(avaiable, break_moment[0], repaird_moment, to_repair, sim_time)
 
     return sim_time
 
 def sistema_rep_ej1(N, S):
     return sistema_rep_gen(N,S,1)
+
+# Calculo esperanza, variacion y desviacion estandar de los distintos casos de estudio
 
 e = 0
 sum_cuadrados = 0
@@ -62,6 +61,7 @@ for _ in range(n_sim):
     sum_cuadrados += res_sim**2
 esperanza = e/ n_sim
 varianza = (sum_cuadrados/n_sim - esperanza**2)
+print("Caso de estudio con 1 operarios, 7 maquinas en uso y 3 repuestos")
 print(f"Esperanza: {esperanza}")
 print(f"Varianza: {varianza}")
 print(f"Desviacion estandar: {np.sqrt(varianza)}")
@@ -71,17 +71,31 @@ print('------------------------')
 
 e = 0
 sum_cuadrados = 0
-n_sim = 10_000
 for _ in range(n_sim):
     res_sim = sistema_rep_gen(7, 3, 2)
     e += res_sim
     sum_cuadrados += res_sim**2
 esperanza = e/ n_sim
 varianza = (sum_cuadrados/n_sim - esperanza**2)
-print("Metodo generico")
+print("Caso de estudio con 2 operarios, 7 maquinas en uso y 3 repuestos")
 print(f"Esperanza: {esperanza}")
 print(f"Varianza: {varianza}")
 print(f"Desviacion estandar: {np.sqrt(varianza)}")
 print('------------------------')
+
+e = 0
+sum_cuadrados = 0
+for _ in range(n_sim):
+    res_sim = sistema_rep_gen(7, 4, 1)
+    e += res_sim
+    sum_cuadrados += res_sim**2
+esperanza = e/ n_sim
+varianza = (sum_cuadrados/n_sim - esperanza**2)
+print("Caso de estudio con 1 operarios, 7 maquinas en uso y 4 repuestos")
+print(f"Esperanza: {esperanza}")
+print(f"Varianza: {varianza}")
+print(f"Desviacion estandar: {np.sqrt(varianza)}")
+print('------------------------')
+
 
 
